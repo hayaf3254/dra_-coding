85a4452935f9df784dc4490bac9943df29792dbd
hayaf3254 <poso42731427@gmail.com>
2025-08-16 19:56:28 +0900

    1→4のみDFSをつくった

--- DFS.py
+++ DFS.py
@@ -1,5 +1,4 @@
 import sys
-
 graph = {}
 
 for line in sys.stdin:
@@ -7,7 +6,6 @@ for line in sys.stdin:
     start = int(start)
     end = int(end)
     dist = float(dist)
-
     if start not in graph:
         graph[start] = []
     graph[start].append((end, dist))
@@ -20,17 +18,13 @@ goal = 4
 def dfs(current, path, total_dist):
     visited[current] = True
     path.append(current)
-
     if current == goal:
         print(path, total_dist)  # 経路と距離を出力
-        return path,total_dist
+        return path, total_dist
     else:
         for nxt, dist in graph[current]:
-            print(graph[current])
             if not visited[nxt]:
-                print(nxt,path,dist)
                 dfs(nxt, path, total_dist + dist)
-
     # バックトラッキング
     path.pop()
     visited[current] = False
